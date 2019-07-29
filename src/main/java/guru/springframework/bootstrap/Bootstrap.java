b4944d50f2a1f384a2e89ca0d86ca217d5531700
Kyle Schafer <kschay@protonmail.com>
2019-07-29 14:44:27 -0500

    Closes #8 - Added Customer to Bootstrap Data Loader

--- src/main/java/guru/springframework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/bootstrap/Bootstrap.java
@@ -1,7 +1,9 @@
 package guru.springframework.bootstrap;
 
 import guru.springframework.domain.Category;
+import guru.springframework.domain.Customer;
 import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +14,23 @@ import java.util.List;
 public class Bootstrap implements CommandLineRunner {
 
     private CategoryRepository categoryRepository;
+    private CustomerRepository customerRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository) {
+    public Bootstrap(CategoryRepository categoryRepository,
+                     CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        //load categories
-        List<Category> categories;
-        categories = Arrays.asList(
+        loadCategories();
+        loadCustomers();
+
+    }
+
+    private void loadCategories() {
+        List<Category> categories = Arrays.asList(
                 new Category("Fruits"),
                 new Category("Dried"),
                 new Category("Fresh"),
@@ -34,5 +43,21 @@ public class Bootstrap implements CommandLineRunner {
         }
 
         System.out.println("Loaded Category Count: " + categoryRepository.count());
+    }
+
+    private void loadCustomers() {
+        List<Customer> customers = Arrays.asList(
+                new Customer("Evan", "Johnson"),
+                new Customer("Barack", "Obama"),
+                new Customer("Hillary", "Clinton"),
+                new Customer("Donald", "Duck"),
+                new Customer("Mickey", "Mouse")
+        );
+
+        for(Customer customer : customers) {
+            customerRepository.save(customer);
+        }
+
+        System.out.println("Loaded Customer Count: " + customerRepository.count());
     }
 }
