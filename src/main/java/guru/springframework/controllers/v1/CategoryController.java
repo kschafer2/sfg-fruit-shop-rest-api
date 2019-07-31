71117d9a3bf00d4dca6815a6eb8dab3b8d1f62c9
Kyle Schafer <kschay@protonmail.com>
2019-07-31 00:16:08 -0500

    Closes #18 - Refactored Category Controller to RestController

--- src/main/java/guru/springframework/controllers/v1/CategoryController.java
+++ src/main/java/guru/springframework/controllers/v1/CategoryController.java
@@ -5,14 +5,10 @@ import guru.springframework.api.v1.model.CategoryListDto;
 import guru.springframework.services.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/api/v1/categories")
 public class CategoryController {
 
@@ -23,22 +19,18 @@ public class CategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<CategoryListDto> getAllCategories() {
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryListDto getAllCategories() {
         log.info("Getting All Categories...");
 
-        return new ResponseEntity<>(
-                new CategoryListDto(categoryService.getAllCategories()),
-                HttpStatus.OK
-        );
+        return new CategoryListDto(categoryService.getAllCategories());
     }
 
     @GetMapping("{name}")
-    public ResponseEntity<CategoryDto> getCategoryByName(@PathVariable String name) {
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDto getCategoryByName(@PathVariable String name) {
         log.info("Getting Category: " + name);
 
-        return new ResponseEntity<>(
-                categoryService.getCategoryByName(name),
-                HttpStatus.OK
-        );
+        return categoryService.getCategoryByName(name);
     }
 }
